1e1e64e8715d0f0fd6afa0df0489263542aeaecc
alswoodev <alswoo.dev@gmail.com>
2026-01-31 05:30:18 +0000

    chore: edit prompt and user text for test

--- basic/src/test/java/com/spring/ai/basic/Agent/MailAgentTest.java
+++ basic/src/test/java/com/spring/ai/basic/Agent/MailAgentTest.java
@@ -34,12 +34,12 @@ public class MailAgentTest {
 
     @Test
     public void processTest() {
-        String userMessage = "{your_mail}으로 '안녕하세요'라는 제목과 함께 새해 안부를 묻는 내용의 메일 보내줘";
-        String userMessage2 = "최근 메일들을 모두 검색해서 '프론트엔드'라는 제목의 메일이 있는지 찾아보고, 누구한테 왔는지 확인해줘";
+        //String userMessage = "{your_mail}으로 '안녕하세요'라는 제목과 함께 새해 안부를 묻는 내용의 메일 보내줘";
+        String userMessage2 = "최근 메일을 모두 검색한 다음에 요약해줘";
         String userId = testUser.getUserId().toString();
 
-        String response = mailAgent.process(userMessage, userId);
-        System.out.println("Agent Response: " + response);
+        //String response = mailAgent.process(userMessage, userId);
+        //System.out.println("Agent Response: " + response);
 
         String response2 = mailAgent.process(userMessage2, userId);
         System.out.println("Agent Response: " + response2);
